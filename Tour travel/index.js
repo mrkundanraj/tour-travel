94c524b1de8f77bdf49b1efb6a19092dae192d86
kundan kumar <mr.kundann.raj@gmail.com>
2026-02-12 00:17:21 +0530

    Delete Tour travel directory

--- Tour travel/index.js
+++
@@ -1,8 +0,0 @@
-let currentSlide = 0;
-
-setInterval(() => {
-  currentSlide = (currentSlide + 1) % 3;
-  document.querySelector(`#slide${currentSlide + 1}`).style.left = '0';
-  document.querySelector(`#slide${(currentSlide + 2) % 3 + 1}`).style.left = '100%';
-  document.querySelector(`#slide${(currentSlide + 1) % 3 + 1}`).style.left = '200%';
-}, 3000); /* adjust the interval as needed */
